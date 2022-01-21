86d65726479639ebc1ef141cde068b11e79c178d
Shinkarenko Alexey <alex.shinkarenko17@gmail.com>
2022-01-22 02:58:36 +0300

    WelcomeImage

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -18,8 +21,14 @@ client.on("messageCreate", (message) => {
     }
 })
 
-client.on('guildMemberAdd', (member) => {
-    
+const welcomeChannelId = "934227813081428038"
+
+client.on('guildMemberAdd', async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server`,
+        files: [img]
+    })
 });
 
 client.login(process.env.TOKEN)
